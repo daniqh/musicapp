1cd5aef4734224c5dc4bf7b61549e6dea7f78992
daniqh <dani.q.h@gmail.com>
2016-03-31 11:03:24 +0100

    Update 20160323182524_create_playlists.rb

--- db/migrate/20160323182524_create_playlists.rb
+++ db/migrate/20160323182524_create_playlists.rb
@@ -1,10 +1,10 @@
 class CreatePlaylists < ActiveRecord::Migration
   def change
     create_table :playlists do |t|
-    	
-     	t.string :name
-      	t.integer :number_of_votes
-     	t.timestamps null: false
-   
+      t.string :name
+      t.string :number_of_votes
+
+      t.timestamps null: false
+    end
   end
 end
